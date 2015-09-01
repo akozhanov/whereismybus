9e7f0fdc4b52b8d818e332c303073f80fd205a6a
Andrew Kozhanov <andrew.kozhanov@gmail.com>
2015-09-01 17:51:17 +0300

    adding modules for the remaining components

---
+++ clientservice/src/main/java/com/wimb/clientservice/TempClass.java
@@ -0,0 +1,7 @@
+package com.wimb.clientservice;
+
+/**
+ * Created by akozhanov on 9/1/2015.
+ */
+public class TempClass {
+}
---
+++ dataservice/src/main/java/com/wimb/dataservice/TempClass.java
@@ -0,0 +1,7 @@
+package com.wimb.dataservice;
+
+/**
+ * Created by akozhanov on 9/1/2015.
+ */
+public class TempClass {
+}
