b7566989b598a4ce0b28d84deeec13547a2105fb
Andrew Kozhanov <andrew.kozhanov@gmail.com>
2016-02-25 18:14:27 +0200

    test message

--- deploy.bat
+++ deploy.bat
@@ -2,3 +2,4 @@ pscp -i C:\works\learn\gps\testserver.ppk .\distribution\target\distribution-1.0
 pscp -r -i C:\works\learn\gps\testserver.ppk .\src\db ec2-user@54.153.42.142:/home/ec2-user/wimb
 
 modified
+modified again
