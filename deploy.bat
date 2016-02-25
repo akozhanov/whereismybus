3dc098d1341a21e9280f3e1b49aebef37b2f63a0
Andrew Kozhanov <andrew.kozhanov@gmail.com>
2016-02-25 18:12:00 +0200

    test commit

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,4 @@
 pscp -i C:\works\learn\gps\testserver.ppk .\distribution\target\distribution-1.0-SNAPSHOT-bin\*.* ec2-user@54.153.42.142:/home/ec2-user/wimb
 pscp -r -i C:\works\learn\gps\testserver.ppk .\src\db ec2-user@54.153.42.142:/home/ec2-user/wimb
+
+modified
