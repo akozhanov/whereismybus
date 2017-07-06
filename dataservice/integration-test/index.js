c3ae613e07ccfc686262f9edae433e4384159170
akozhanov <andrew.kozhanov@gmail.com>
2017-07-06 22:38:22 +0300

    integration tests using nodejs

---
+++ dataservice/integration-test/index.js
@@ -0,0 +1,6 @@
+var test = require('tape');
+
+test('basic', function (t){
+    t.equal(2+3,6,'not equal');
+    t.end();
+})
--- dataservice/integration-test/test.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import time
-
-print("waiting for dataservice to start")
-time.sleep(100)
-print("sending test request")
-urllib.request.urlopen("http://wimb-dataservice:10000/data/226?time=2015-09-03T10:16:50Z&lat=50.5174202&long=30.4485796&s=0.0&dir=0.0&sat=25&alt=0.0&acc=26.23900032043457&prov=network&batt=28.0&aid=8a53003a7f32ce99&ser=LGD856cd544cf6").read()
-print("done. press enter")
-input("Press Enter to continue...")
