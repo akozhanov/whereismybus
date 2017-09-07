67324e896e62e1357c60c3925e5cd965ebd2655b
akozhanov <FinalTrack6>
2017-09-07 15:24:08 +0300

    ensuring proper cleanup during test & build

--- dataservice/src/main/java/com/wimb/dataservice/DataService.java
+++ dataservice/src/main/java/com/wimb/dataservice/DataService.java
@@ -55,7 +55,7 @@ public class DataService {
         HttpServer server = HttpServer.create(new InetSocketAddress(10000), 0);
         server.createContext("/data/226", new DataHandler());
         server.setExecutor(null); // creates a default executor
-        logger.info("DATA SERVICE HAS BEEN STARTED - UPDATED");
+        logger.info("DATA SERVICE HAS BEEN STARTED - UPDATED - AND ONE MORE UPDATE");
         server.start();
     }
 
